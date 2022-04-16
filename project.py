7ad7ac256600aad0f3554bca8f0ed81d828dfa71
yeabsira <yobo1889@protonmail.com>
2022-04-16 17:21:24 -0500

    draft-1

--- project.py
+++ project.py
@@ -1,44 +1,57 @@
-filepath = 'h1b_datahubexport-2018.csv';
-database = open(filepath,"r");
+import helper
+filePath = 'dummyData.csv';
+database = open(filePath,"r");
+database.readline();
+
+
+visaData = helper.readFile(filePath);
+
 
 def getCompaniesByState(state):
-   substring = "\"" + state + "\"";
-   for i in range(1000):
-       lineData = database.readline()
-       databaseInfo = lineData.split(",");
-       if(databaseInfo[8]==substring):
-         print(databaseInfo[1]);
+   prevCompany = ""
+   companies = "";
+   for j in visaData:
+        for i in range(60):
+         currCompany = visaData[j]["2018"]["Employer"];
+         if(visaData[j]["2018"]["State"]==state) & (prevCompany!=currCompany):
+            companies = companies + visaData[j]["2018"]["Employer"] + "\n";
+            prevCompany = currCompany;
+            
+   return companies    
+        
 
 def getStatByCompanies(company):
-     substring = "\"" + company + "\"";
-     for i in range(1000):
-        lineData = database.readline()
-        databaseInfo = lineData.split(",");
-        if(databaseInfo[1]==substring):
-            print("Fiscal Year => " + databaseInfo[0]);
-            print("Employer => " + databaseInfo[1]);
-            print("Initial Approvals => " + databaseInfo[2]);
-            print("Initial Denials => " + databaseInfo[3]);
-            print("Continuing Approvals => " + databaseInfo[4]);
-            print("Continuing Denials => " + databaseInfo[5]);
-            print("NAICS => " + databaseInfo[6]);
-            print("Tax ID => " + databaseInfo[7]);
-            print("State => " + databaseInfo[8]);
-            print("City => " + databaseInfo[9]);
-            print("ZIP => " + databaseInfo[10]);
+     statsForCompany = ""
+     years = {"2018","2019","2020"}
+     for j in visaData:
+         for i in years:
+            if(j == company):
+                statsForCompany = statsForCompany + "\nFiscal Year => " + visaData[j][i]["Fiscal Year"]  + "\n";
+                statsForCompany = statsForCompany + "Employer => " + visaData[j][i]["Employer"]  + "\n";
+                statsForCompany = statsForCompany + "Initial Approvals => " + visaData[j][i]["Initial Approvals"] + "\n";
+                statsForCompany = statsForCompany + "Initial Denials => " + visaData[j][i]["Initial Denials"] + "\n";
+                statsForCompany = statsForCompany + "Continuing Approvals => " + visaData[j][i]["Continuing Approvals"] + "\n";
+                statsForCompany = statsForCompany + "Continuing Denials => " + visaData[j][i]["Continuing Denials"] + "\n";
+                statsForCompany = statsForCompany + "NAICS => " + visaData[j][i]["NAICS"] + "\n";
+                statsForCompany = statsForCompany + "Tax ID => " + visaData[j][i]["Tax ID"] + "\n";
+                statsForCompany = statsForCompany + "State => " +visaData[j][i]["State"] + "\n";
+                statsForCompany = statsForCompany + "City => " + visaData[j][i]["City"] + "\n";
+                statsForCompany = statsForCompany + "ZIP => " + visaData[j][i]["ZIP"] + "\n";
+
+     return statsForCompany;
+
 
 def getCompaniesByMinInitApproval(initApproval):
-    substring = initApproval;
-    database.readline();
-    for i in range(10000):
-        lineData = database.readline()
-        databaseInfo = lineData.split(",");
-        if(int(databaseInfo[2].replace("\"",""))>=initApproval):
-            print("Employer => " + databaseInfo[1]);
+    companies  = "";
+    years = {"2018","2019","2020"}
+    for j in visaData:
+        for i in years:
+            if(int(visaData[j][i]["Initial Approvals"]) >= initApproval):
+                companies = companies + visaData[j][i]["Employer"] + " in Year " + visaData[j][i]["Fiscal Year"] + "\n";
+    return companies;            
 
-
-         
-
-#getStatByCompanies("CVE TECHNOLOGY GROUP INC");
-#getCompaniesByState("TX");
-getCompaniesByMinInitApproval(2);
+        
+#print(helper.readFile(filePath));
+#print(getStatByCompanies("GLOBAL TAX NETWORK ATLANTIC LLC"));
+#print(getCompaniesByState("CA"));
+#print(getCompaniesByMinInitApproval(0));
